0b4a2d2a536f234d3a221a342c206f21caf8b30f
iferraro <afronerd01@outlook.com>
2024-08-30 19:42:52 -0700

    More refined table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,24 +13,31 @@ export default function IndexPage() {
 
   const colorRows = colors.map((c) => {
     return (
-      <tr>
-        <td>{c.name}</td>
-        <td>{c.hex}</td>
+      <tr className="border-2 border-white text-white">
+        <td className="p-4 pr-2">
+          <div className="w-12 h-6" style={{ background: c.hex }} />
+        </td>
+        <td className="p-4 pl-2">{c.name}</td>
+        <td className="p-4">{c.hex}</td>
       </tr>
     );
   });
 
   return (
-    <main className="flex justify-center bg-blue-700">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Hex</th>
-          </tr>
-        </thead>
-        <tbody>{colorRows}</tbody>
-      </table>
+    <main className="flex justify-center items-center bg-black">
+      <section className="flex flex-col justify-center max-w-96 bg-black">
+        <h1 className="text-white">Isaac's Custom Colors</h1>
+        <table className="border-2 border-white">
+          <thead>
+            <tr className="text-white">
+              <th>Box</th>
+              <th>Name</th>
+              <th>Hex</th>
+            </tr>
+          </thead>
+          <tbody>{colorRows}</tbody>
+        </table>
+      </section>
     </main>
   );
 }
